f01b71443569d4ddc1d3e7f183a64a1a418fd212
iqbalmdkaify <iqbalmdkaify81@gmail.com>
2025-04-01 23:55:57 +0200

    fixed NotFound Page UI

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const NotFoundPage = () => {
   const location = useLocation();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [error404, setError404] = useState<string | null>(null);
 
   const visitedpath = location.pathname.slice(1,);
@@ -12,7 +11,7 @@ const NotFoundPage = () => {
   useEffect(() => {
     const fetchNotFoundMessage = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_ROOT_API}${visitedpath}`);
+        const response = await axios.get(`${import.meta.env.VITE_ROOT_API}/${visitedpath}`);
         setError404(response.data.message);
       } catch (error) {
         console.log(error);
@@ -20,14 +19,15 @@ const NotFoundPage = () => {
     };
 
     fetchNotFoundMessage();
-  }, []);
+  }, [visitedpath]);
 
   return (
-    <div className='not-found-wrapper'>
-      <h1 className='not-found-title'>Page Not Found 404!</h1>
-      {/* <p className='not-found-message'>{error404}</p> */}
-      <p className='not-found-message'>Unexpected error ocurred</p>
-      <Link to="/" className='not-found-link' >Go to Home Page</Link>
+    <div className='w-screen h-screen bg-light flex justify-center items-center'>
+      <div className='flex flex-col gap-4'>
+        <h1 className='font-semibold text-3xl'>Page Not Found 404!</h1>
+        <p className='text-base italic'>{`Unexpected error ocurred!, ${error404}`}</p>
+        <Link to="/" className='text-blue-300 font-medium underline' >Go to Home Page</Link>
+      </div>
     </div>
   )
 }
