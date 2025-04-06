691c447fd3a38c8f9f7374c492ed823f63d56780
iqbalmdkaify <iqbalmdkaify81@gmail.com>
2025-04-06 20:14:51 +0200

    Commented the file wherever necessary

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,17 +1,21 @@
 const { BlogModel } = require("../models/dataModel");
 
+// Fetch all blogs from the database
 const getAllBlogs = async (req, res) => {
 	try {
-		const blogs = await BlogModel.find().lean();
+		const blogs = await BlogModel.find().lean(); // lean() returns plain JS objects instead of Mongoose documents
 		res.status(200).json(blogs);
 	} catch (err) {
 		res.status(500).json("Unexpected Error");
 	}
 };
 
+// Create a new blog post
 const createBlog = async (req, res) => {
 	const { title, content, author } = req.body;
 	const image = req.file;
+
+	// Create new blog instance with image data
 	const blog = await BlogModel({
 		title,
 		content,
@@ -22,31 +26,33 @@ const createBlog = async (req, res) => {
 			data: image.buffer,
 		},
 	});
-	try {
-		const savedBlog = await blog.save();
 
+	try {
+		const savedBlog = await blog.save(); // Save blog to the database
 		res.status(201).json({ success: true, _id: savedBlog._id });
 	} catch (err) {
 		res.status(500).json({ success: false, message: "Error saving the blog form" });
 	}
 };
 
+// Fetch a single blog by ID
 const getSingleBlog = async (req, res) => {
 	const blogId = req.params.id;
-	try {
-		const blog = await BlogModel.findById(blogId).lean();
 
+	try {
+		const blog = await BlogModel.findById(blogId).lean(); // Fetch blog using ID
 		res.status(200).json(blog);
 	} catch (error) {
 		res.status(500).json("Unexpected Error");
 	}
 };
 
+// Delete a blog by ID
 const deleteBlog = async (req, res) => {
 	const blog_id = req.params.id;
 
 	try {
-		const deletedBlog = await BlogModel.findByIdAndDelete(blog_id);
+		const deletedBlog = await BlogModel.findByIdAndDelete(blog_id); // Delete blog by ID
 
 		if (!deletedBlog) {
 			return res.status(404).json({ message: "Blog not found" });
